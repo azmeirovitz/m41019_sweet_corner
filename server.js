6cf4d9883684262446e8a3c1153f840aad8bebbe
Ariel <arcomp123@gmail.com>
2019-11-30 17:04:53 -0800

    Setup DB and completed get all products endpoint

--- server.js
+++ server.js
@@ -1,13 +1,39 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
+const db = require('./db');
+const {buildUrl} = require('./helpers')
 
 const app = express();
 
-app.get('/test', (req,res) => {
-    res.send({
-        message: 'This is a test endpoint, if you see this, then it is working'
+app.get('/api/products', async (req,res) => {
+
+    //buildUrl(req);
+
+    const [results] = await db.query(`
+    SELECT p.pid AS id, caption, cost, p.name, i.pid AS thId, altText, file, type
+    FROM products AS p JOIN images AS i ON i.productID=p.id WHERE i.type='thumbnail'
+    `);
+
+    const products = results.map(product => {
+        const { thId, altText, file, type, ...p } = product;
+
+        return {
+            ...p,
+            thumbnail: {
+                id: thId,
+                altText: altText,
+                file: file,
+                type: type,
+                url: buildUrl(req, type, file)
+            }
+        }
     });
-});
+
+    res.send({ products: products});
+        //message: 'This is to get all products',
+        //products: products
+    });
+//});
 
 app.listen(PORT, () => {
     console.log('The server is running at localhost' + PORT);
