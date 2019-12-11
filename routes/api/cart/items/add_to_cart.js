6d3f68516ee86fdc6e642cc727623989b5dee5a5
Ariel <arcomp123@gmail.com>
2019-12-10 21:02:13 -0800

    Basic add to cart functionality working. Next step is to build cert midlleware

--- routes/api/cart/items/add_to_cart.js
+++ routes/api/cart/items/add_to_cart.js
@@ -1,10 +1,22 @@
 const db = require('../../../../db');
+const cartJwt = require('../../../../config/cart_jwt.json');
+const jwt = require('jwt-simple');
 
 module.exports = async (req, res) => {
 
     // Check for existing cart
+    const {product_id} = req.params; //Will pull acctual info from url
 
     let {cart} = req;
+    let token = null;
+
+    //Checking if product_id is valid
+    const [[product = null]] = await db.execute('SELECT id, name FROM products WHERE pid=?', [product_id]);
+
+    if (!product) {
+        res.status(404).send('Product not found');
+        return;
+    }
     
     //If no cart, create a new cart
     if (!cart) {
@@ -13,20 +25,40 @@ module.exports = async (req, res) => {
 
         const [result] = await db.query('INSERT INTO cart (pid, statusId) VALUES (UUID(), ?)', [cartStatus.id]);
 
-        console.log('Results: ', result);
+        cart = {
+            id: result.insertId
+        };
+
+        const tokenData = {
+            cartId: cart.id,
+            ts: Date.now()   //ts is timestamp
+        };
+
+        token = jwt.encode(tokenData, cartJwt.secret);
 
     }
 
     // Does item already exist in cart
 
-    //If product already in cart, then increase quantity 
+    const [[cartItem = null]] = await db.query('SELECT id FROM cartItems WHERE cartId=? AND productId=?', [cart.id, product.id]);
 
-    // Else, create cart item for product
+    //If product already in cart, then increase quantity 
+    if (cartItem) {
+        //Increase the quantity of the existing cartItem
+    } else {
+        // Else, create cart item for product
+        const [itemResult] = await db.execute('INSERT INTO cartItems (pid, cartId, productId, quantity) VALUES (UUID(), ?, ?, ?)', [cart.id, product.id, 1])
+    }
+
+    const message = `1 ${product.name} added to cart`  
+
+    
 
     // Create a message to send back to user of what was added
 
     res.send ({
-        message: 'This will add an item to cart comeday'
+        cartToken: token,
+        message: message
     });
 }
 
